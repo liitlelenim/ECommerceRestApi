5f68daf7baf233818a0ffd61a651592a23b5aa97
liitlelenim <jakubblaszczyk20@gmail.com>
2022-04-08 17:40:28 +0200

    create product entity

--- ECommerceRestApi/DAL/AppDbContext.cs
+++ ECommerceRestApi/DAL/AppDbContext.cs
@@ -6,7 +6,8 @@ namespace ECommerceRestApi.DAL;
 public class AppDbContext : DbContext
 {
     public DbSet<AppUser> AppUsers { get; set; }
-
+    public DbSet<Product> Products { get; set; }
+    
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
---
+++ ECommerceRestApi/Entities/Product.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using CSharpVitamins;
+
+namespace ECommerceRestApi.Entities;
+
+public class Product
+{
+    [Key] public int Id { get; set; }
+    public string Uri { get; set; } = ShortGuid.NewGuid();
+    public string Name { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
+    public decimal Price { get; set; }
+}
